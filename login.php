97ab8e0f68d2e4a8b62a67cb1ab74b4f6e753836
harishv0 <harishv2505@gmail.com>
2024-12-21 23:48:43 +0530

    first commit

--- login.php
+++ login.php
@@ -12,9 +12,9 @@
     
 
     try {
-        $dsn = "mysql:host=sql211.infinityfree.com;port=3306;dbname=if0_37946139_taskmanagement";
-        $username = "if0_37946139";
-        $password = "HXobTcuXon59";
+        $dsn = "mysql:host=sql12.freesqldatabase.com;port=3306;dbname=sql12752944";
+        $username = "sql12752944";
+        $password = "9w5Eq9LF97";
 
         // Create a PDO instance
         $conn = new PDO($dsn, $username, $password);
