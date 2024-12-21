82312ff0eb633aef074213173cff9c889cba67e8
harishv0 <harishv2505@gmail.com>
2024-12-21 23:45:26 +0530

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM php:8.1-apache
+RUN docker-php-ext-install pdo pdo_mysql
 COPY . /var/www/html
 EXPOSE 80
-CMD ["php", "-S", "0.0.0.0:80"]
+CMD ["php", "-S", "0.0.0.0:80", "-t", "/var/www/html"]
--- index.php
+++ index.php
@@ -14,29 +14,11 @@ try {
     $username = "sql12752944";
     $password = "9w5Eq9LF97";
 
-    // Create a PDO instance
     $conn = new PDO($dsn, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connection successful!";
-   if($_SERVER['REQUEST_METHOD'] === 'GET'){
-    fetchAllUsers($conn);
-   }
    
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
-}
-
-function fetchAllUsers($conn){
-    $res = $conn->prepare("SELECT * FROM user WHERE role = :role");
-    $res->bindValue(":role", "employee", PDO::PARAM_STR);
-    $res->execute();
-
-    if($res->rowCount() > 0){
-        $data = $res->fetchAll(PDO::FETCH_ASSOC);
-        
-        echo json_encode(["status" => true,"message" => "fetched all users","data" => $data]);
-    } else {
-        echo json_encode(["status" => false,"message" => "no users found"]);
-    }
 }
 ?>
